62bd39a63d8b05757fdb8a29a755e5e16bd6f71b
asallaup <arvid.sallaup@gmail.com>
2024-02-05 21:54:41 +0100

    First class

--- src/main/java/no/miles/generic/SimpleExample.java
+++ src/main/java/no/miles/generic/SimpleExample.java
@@ -1,4 +1,45 @@
 package no.miles.generic;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class SimpleExample {
+
+    // Java 1.4
+    public void oldFashionWithClassCast() {
+        List list = new ArrayList();
+        list.add("String 1");
+        list.add("String 2");
+        for (int i = 0; i < list.size(); i++) {
+            String s = (String) list.get(0);
+            System.out.println(s);
+        }
+    }
+
+    // Java 1.5
+    public void withGeneric() {
+        List<String> list = new ArrayList<String>();
+        list.add("String 1");
+        list.add("String 2");
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    // Java 8
+    public void withLambda() {
+        List<String> list = new ArrayList<>();
+        list.add("String 1");
+        list.add("String 2");
+        list.forEach(System.out::println);
+    }
+
+    // Java 10
+    public void withVar() {
+        var list = new ArrayList<String>();
+        list.add("String 1");
+        list.add("String 2");
+        list.forEach(System.out::println);
+    }
 }
